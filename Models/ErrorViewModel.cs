88417f369d658a44e995b99e1f7ed6451a9a23fc
Cody Fortenberry <codyfortenberry1@gmail.com>
2022-07-26 09:47:23 -0500

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WidaUserDirectory.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
