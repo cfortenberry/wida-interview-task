1eb57ad6345ca7989c8eb5cd323dede2406edf9f
Cody Fortenberry <codyfortenberry1@gmail.com>
2022-07-27 18:36:30 -0500

    Added user table and detail page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,14 +13,117 @@ namespace WidaUserDirectory.Controllers
             _logger = logger;
         }
 
+        // GET: Users
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<UserModel>? users = null;
+
+            using(var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://jsonplaceholder.typicode.com/");
+                //HTTP GET
+                var responseTask = client.GetAsync("users");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<IList<UserModel>>();
+                    readTask.Wait();
+
+                    users = readTask.Result;
+                }
+                else
+                {
+                    users = Enumerable.Empty<UserModel>();
+
+                    ModelState.AddModelError(string.Empty, "Server error");
+                }
+            }
+
+            return View(users);
         }
 
-        public IActionResult Privacy()
+        public IActionResult UserDetail(int id)
         {
-            return View();
+            UserDetailModel userDetail = new UserDetailModel();
+
+            // get user info
+            userDetail.User = GetUser(id);
+            // get user todo list
+            userDetail.ToDo = GetToDos(id);
+
+            return View(userDetail);
+        }
+
+        public UserModel GetUser (int id)
+        {
+            UserModel user = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://jsonplaceholder.typicode.com/");
+                //HTTP GET
+                var responseTask = client.GetAsync("users/" + id.ToString());
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<UserModel>();
+                    readTask.Wait();
+
+                    user = readTask.Result;
+                }
+            }
+
+            return user;
+        }
+
+        public IEnumerable<ToDo> GetToDos (int id)
+        {
+            IEnumerable<ToDo> toDos = null;
+
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://jsonplaceholder.typicode.com/");
+                //HTTP GET
+                var responseTask = client.GetAsync("users/" + id.ToString() + "/todos");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var todoReadTask = result.Content.ReadAsAsync<IList<ToDo>>();
+                    todoReadTask.Wait();
+
+                    toDos = todoReadTask.Result;
+                }
+            }
+
+            return toDos;
+        }
+
+        [HttpPost]
+        public ActionResult userDetail(UserModel user)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://jsonplaceholder.typicode.com/");
+
+                //HTTP POST
+                var putTask = client.PutAsJsonAsync<UserModel>("user", user);
+                putTask.Wait();
+
+                var result = putTask.Result;
+                //if (result.IsSuccessStatusCode)
+                //{
+
+                    return RedirectToAction("Index");
+                //}
+            }
+            return View(user);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ Models/UserModel.cs
@@ -0,0 +1,48 @@
+﻿namespace WidaUserDirectory.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Username { get; set; }
+        public string? Email { get; set; }
+        public Company? Company { get; set; }
+        public Address? Address { get; set; }
+        public string? Phone { get; set; }
+
+        public string? CompanyName 
+        { 
+            get { return Company?.Name; } 
+        }
+        public string? City
+        {
+            get { return Address?.City; }
+        }
+    }
+
+    public class UserDetailModel
+    {
+        public UserModel User { get; set; }
+        public IEnumerable<ToDo>? ToDo { get; set; }
+    }
+
+    public class Address
+    {
+        public string? Street { get; set; }
+        public string? Suite { get; set; }
+        public string? City { get; set; }
+        public string? ZipCode { get; set; }
+    }
+
+    public class ToDo
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public bool Completed { get; set; }
+    }
+
+    public class Company
+    {
+        public string? Name { get; set; }
+    }
+}
